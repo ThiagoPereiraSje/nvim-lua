417a92b7252516faf4d4f2f9f3721f71c5e714d4
Thiago Pereira <thiagopereiramail@gmail.com>
2022-09-17 11:11:17 +0000

    Remove do arquivo keymaps as teclas de atalho do lsp

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -16,15 +16,3 @@ vim.api.nvim_set_keymap('t', '<c-left>', '<c-\\><c-n>:FloatermPrev<CR>', { norem
 vim.api.nvim_set_keymap('t', '<c-right>', '<c-\\><c-n>:FloatermNext<CR>', { noremap=true })
 vim.api.nvim_set_keymap('t', '<c-j>', '<c-\\><c-n>:FloatermToggle<CR>', { noremap=true })
 
--- lsp
-vim.api.nvim_set_keymap('n', 'gd', ':lua vim.lsp.buf.definition()<cr>', { noremap = true })
-vim.api.nvim_set_keymap('n', 'gD', ':lua vim.lsp.buf.declaration()<cr>', { noremap = true })
-vim.api.nvim_set_keymap('n', 'gi', ':lua vim.lsp.buf.implementation()<cr>', { noremap = true })
-vim.api.nvim_set_keymap('n', 'gw', ':lua vim.lsp.buf.document_symbol()<cr>', { noremap = true })
-vim.api.nvim_set_keymap('n', 'gw', ':lua vim.lsp.buf.workspace_symbol()<cr>', { noremap = true })
-vim.api.nvim_set_keymap('n', 'gr', ':lua vim.lsp.buf.references()<cr>', { noremap = true })
-vim.api.nvim_set_keymap('n', 'gt', ':lua vim.lsp.buf.type_definition()<cr>', { noremap = true })
-vim.api.nvim_set_keymap('n', 'K', ':lua vim.lsp.buf.hover()<cr>', { noremap = true })
-vim.api.nvim_set_keymap('n', '<c-k>', ':lua vim.lsp.buf.signature_help()<cr>', { noremap = true })
-vim.api.nvim_set_keymap('n', '<leader>ca', ':lua vim.lsp.buf.code_action()<cr>', { noremap = true })
-vim.api.nvim_set_keymap('n', '<leader>rn', ':lua vim.lsp.buf.rename()<cr>', { noremap = true })
