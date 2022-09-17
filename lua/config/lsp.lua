2363771b2bb5953c2ba5a977e44f9cf07ff93634
Thiago Pereira <thiagopereiramail@gmail.com>
2022-09-17 11:24:24 +0000

    Adiciona as configuracoes da janela do lsp

--- lua/config/lsp.lua
+++ lua/config/lsp.lua
@@ -1,4 +1,17 @@
+local cmp = require('cmp')
+local lsp_status = require('lsp-status')
+local lsp_win = require('lspconfig.ui.windows')
 local lsp_installer = require('nvim-lsp-installer')
+
+-- lsp windows config
+local lsp_win_default_opts = lsp_win.default_opts
+
+lsp_win.default_opts = function(options)
+	local opts = lsp_win_default_opts(options)
+	opts.border = 'rounded'
+
+	return opts
+end
 
 lsp_installer.on_server_ready(function(server)
 	local opts = {}
