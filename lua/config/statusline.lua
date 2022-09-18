e41209a8958e25915bbfceef3bb700d62de4f9b9
Thiago Pereira <thiagopereiramail@gmail.com>
2022-09-18 12:38:55 -0300

    Adiciona um icone de separacao das secoes

--- lua/config/statusline.lua
+++ lua/config/statusline.lua
@@ -113,7 +113,7 @@ require('lualine').setup({
 		theme = custom_theme,
 		icons_enabled = false,
 		component_separators = { left = '', right = '' },
-		section_separators = { left = '', right = '' },
+		section_separators = { left = '', right = '' },
 		disabled_filetypes = { 'dashboard', 'NvimTree', 'Outline' },
 	},
 	sections = {
