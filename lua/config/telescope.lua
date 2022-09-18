29dfb81a4887155c27c3dd04e95e6d03a8d75dd4
Thiago Pereira <thiagopereiramail@gmail.com>
2022-09-18 12:32:42 -0300

    Adiciona algumas teclas de atalho para telescope

--- lua/config/telescope.lua
+++ lua/config/telescope.lua
@@ -13,3 +13,10 @@ require('telescope').setup({
 require('trouble').setup({
 	icons = false
 })
+
+-- Key mappings
+vim.api.nvim_set_keymap('n', '<leader>ff', ':Telescope find_files<CR>', { noremap=true })
+vim.api.nvim_set_keymap('n', '<leader>fg', ':Telescope live_grep<CR>', { noremap=true })
+vim.api.nvim_set_keymap('n', '<leader>fb', ':Telescope buffers<CR>', { noremap=true })
+vim.api.nvim_set_keymap('n', '<leader>fh', ':Telescope help_tags<CR>', { noremap=true })
+
